29ba5cca253b37c2390a3aee3921b8350a680941
Youssef-Rachad <69124029+Youssef-Rachad@users.noreply.github.com>
2020-08-06 18:09:46 -0400

    Update Problem1.py

--- Python/Problem1.py
+++ Python/Problem1.py
@@ -1,1 +1,12 @@
-import numpy
+#https://projecteuler.net/problem=1
+def sumMultiples():
+  sum = 0
+  for i in range(1001):
+    if (i % 3) == 0:
+      sum += i
+      continue
+    if (i % 5) == 0:
+      sum += i
+  return sum
+
+sumMultiples()
